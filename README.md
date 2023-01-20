0e8a45f1da1bc9f8a5354f6d2683cee3b30853cc
CoryMci <corymciver@live.ca>
2023-01-20 10:24:37 -0800

    Clean up debug strings, add README

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # knights-travails
+
+## Live preview
+
+https://corymci.github.io/knights-travails/
+
+## Explanation
+
+The knights-travails project is a challenge provided by The Odin Project, an open-source web development course.
+
+The goal of this challenge is to create a psueudo binary tree for a knight chess piece, create and apply a search algorithm of your choice, and find the shortest path to a given square of the board.
+
+I ended up using a simple breadth-first-search algorithm to find the shortest path, although there are many other more efficient searching algorithms that could be applied in the future.
--- script.js
+++ script.js
@@ -1,122 +1,135 @@
-const knight = function(position = [0,0], path=[]) {
-    let currentPosition = position
-    let currentPath = path.concat([currentPosition])
-    console.log(path)
+const knight = function (position = [0, 0], path = []) {
+  let currentPosition = position;
+  let currentPath = path.concat([currentPosition]);
+  console.log(path);
 
-    const moves = [[-1,-2],[-1,2],[1,-2],[1,2],[-2,-1],[-2,1],[2,-1],[2,1]]
+  const moves = [
+    [-1, -2],
+    [-1, 2],
+    [1, -2],
+    [1, 2],
+    [-2, -1],
+    [-2, 1],
+    [2, -1],
+    [2, 1],
+  ];
 
-    const validMoves = function (){
-        const knights = []
-        moves.forEach(move=> {
-            if (
-                currentPosition[0] + move[0] >= 0 &&
-                currentPosition[0] + move[0] <= 7 &&
-                currentPosition[1] + move[1] >= 0 &&
-                currentPosition[1] + move[1] <= 7
-                ) {
-                    const newposition = [currentPosition[0]+move[0], currentPosition[1]+move[1]] //LEGAL moves
-                    //Make sure new position does not exist in path
-                    const notInPath = (previousPosition) =>  !(previousPosition[0] == newposition[0] && previousPosition[1] == newposition[1])
-                    if (currentPath.every(notInPath
-                       )){
-                        knights.push(knight(newposition, currentPath))
-                    }
-                }
-        })
-        console.log(knights)
-        return knights
-        //return returnArray
-    }
-
-    return {
-        validMoves,
-        get position(){
-            return currentPosition
-        },
-        get path(){
-            return currentPath
+  const validMoves = function () {
+    const knights = [];
+    moves.forEach((move) => {
+      if (
+        currentPosition[0] + move[0] >= 0 &&
+        currentPosition[0] + move[0] <= 7 &&
+        currentPosition[1] + move[1] >= 0 &&
+        currentPosition[1] + move[1] <= 7
+      ) {
+        const newposition = [
+          currentPosition[0] + move[0],
+          currentPosition[1] + move[1],
+        ]; //LEGAL moves
+        //Make sure new position does not exist in previous moves/path
+        const notInPath = (previousPosition) =>
+          !(
+            previousPosition[0] == newposition[0] &&
+            previousPosition[1] == newposition[1]
+          );
+        if (currentPath.every(notInPath)) {
+          knights.push(knight(newposition, currentPath));
         }
-    }
-}
+      }
+    });
+    console.log(knights);
+    return knights;
+    //return returnArray
+  };
 
-const knightMoves = function(currentknight, target = [3,7], queue = []) {
-    if (currentknight.position[0] == target[0] && currentknight.position[1] == target[1]) {
-        console.log('ye');
-        return currentknight
-    } else {
-        queue = queue.concat(currentknight.validMoves())
-        const nextKnight = queue.shift()
-        return knightMoves(nextKnight, target, queue)
-    }
+  return {
+    validMoves,
+    get position() {
+      return currentPosition;
+    },
+    get path() {
+      return currentPath;
+    },
+  };
+};
 
-}
-
+const knightMoves = function (currentknight, target = [3, 7], queue = []) {
+  if (
+    currentknight.position[0] == target[0] &&
+    currentknight.position[1] == target[1]
+  ) {
+    return currentknight;
+  } else {
+    queue = queue.concat(currentknight.validMoves());
+    const nextKnight = queue.shift();
+    return knightMoves(nextKnight, target, queue);
+  }
+};
 
 const Ui = function () {
-    let knightPosition = [0,0]
+  let knightPosition = [0, 0];
 
-    const generateBoard = function(){
-        const board = document.querySelector('.board')
-        for (let y = 0 ; y < 8; y++) {
-            const bar = document.createElement('div')
-            bar.classList.add('bar');
-            for (let x = 0 ; x < 8; x++){
-                const square = document.createElement('div')
-                square.classList.add('square')
-                square.setAttribute('data-x',x)
-                square.setAttribute('data-y',y)
-                square.addEventListener('click', userAction)
-                bar.appendChild(square)
-            }
-            board.prepend(bar);
-        }
-
-        
+  const generateBoard = function () {
+    const board = document.querySelector(".board");
+    for (let y = 0; y < 8; y++) {
+      const bar = document.createElement("div");
+      bar.classList.add("bar");
+      for (let x = 0; x < 8; x++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.setAttribute("data-x", x);
+        square.setAttribute("data-y", y);
+        square.addEventListener("click", userAction);
+        bar.appendChild(square);
+      }
+      board.prepend(bar);
     }
-    const userAction = function(e){
-        let targetX = e.target.dataset.x
-        let targetY = e.target.dataset.y
-        let otherSquares = document.querySelectorAll('.square')
-        otherSquares.forEach(sq => {
-            sq.classList.remove('clicked')
-            sq.classList.remove('path')
-        })
-        e.target.classList.add('clicked')
-        const knightPath = knightMoves(knight(knightPosition), [targetX, targetY]).path
-        console.log(knightPath);
-        knightPosition = knightPath[knightPath.length - 1];
-        console.log(knightPosition);
-        movePiece(knightPath);
-    }
+  };
+  const userAction = function (e) {
+    let targetX = e.target.dataset.x;
+    let targetY = e.target.dataset.y;
+    let otherSquares = document.querySelectorAll(".square");
+    otherSquares.forEach((sq) => {
+      sq.classList.remove("clicked");
+      sq.classList.remove("path");
+    });
+    e.target.classList.add("clicked");
+    const knightPath = knightMoves(knight(knightPosition), [
+      targetX,
+      targetY,
+    ]).path;
+    console.log(knightPath);
+    knightPosition = knightPath[knightPath.length - 1];
+    console.log(knightPosition);
+    movePiece(knightPath);
+  };
 
-    const movePiece = function(array){
-        const knightY = document.querySelector('svg');
-        const knightX = document.querySelector('.knight')
+  const movePiece = function (array) {
+    const knightY = document.querySelector("svg");
+    const knightX = document.querySelector(".knight");
 
+    array.forEach((position, index) => {
+      setTimeout(() => {
+        knightX.style.transform = `translateX(${position[0] * 50}px`;
+      }, index * 2000);
+      setTimeout(() => {
+        knightY.style.transform = `translateY(-${position[1] * 50}px)`;
+      }, index * 2000 + 1000);
+      setTimeout(() => {
+        markPath(position[0], position[1]);
+      }, index * 2000 + 1000);
+    });
+  };
+  const markPath = function (x, y) {
+    let path = document.querySelector(`[data-x="${x}"][data-y="${y}"]`);
+    path.classList.add("path");
+  };
 
-        array.forEach((position, index) => {
-            setTimeout(() => {
-                knightX.style.transform = `translateX(${position[0]*50}px`
-            }, (index*2000));
-            setTimeout(() => {
-                knightY.style.transform = `translateY(-${position[1]*50}px)`
-            }, (index*2000+1000));
-            setTimeout(() => {
-                markPath(position[0], position[1])
-            }, (index*2000+1000))
-            
-        })
-    }
-    const markPath = function (x, y) {
-        let path = document.querySelector(`[data-x="${x}"][data-y="${y}"]`)
-        path.classList.add('path');
-    }
+  return {
+    generateBoard,
+  };
+};
 
-    return {
-        generateBoard
-    }
-
-}
-
-const bort = Ui()
+const bort = Ui();
 bort.generateBoard();
